6162309eef8d5be1648068432b315dcd4dc13d8e
khushthecoder <chaudharikhush0711@gmail.com>
2025-09-30 11:48:46 +0530

    feat: Display product grid on Home screen

---
+++ VISHAL_MART/src/constants/dummyData.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: '1',
+    name: 'Classic White T-Shirt',
+    price: '₹799',
+    image: 'https://picsum.photos/seed/1/400',
+  },
+  {
+    id: '2',
+    name: 'Denim Blue Jeans',
+    price: '₹1,999',
+    image: 'https://picsum.photos/seed/2/400',
+  },
+  {
+    id: '3',
+    name: 'Leather Biker Jacket',
+    price: '₹4,500',
+    image: 'https://picsum.photos/seed/3/400',
+  },
+  {
+    id: '4',
+    name: 'Running Sports Shoes',
+    price: '₹2,250',
+    image: 'https://picsum.photos/seed/4/400',
+  },
+  {
+    id: '5',
+    name: 'Analog Wrist Watch',
+    price: '₹3,100',
+    image: 'https://picsum.photos/seed/5/400',
+  },
+  {
+    id: '6',
+    name: 'Stylish Black Sunglasses',
+    price: '₹1,200',
+    image: 'https://picsum.photos/seed/6/400',
+  },
+];
