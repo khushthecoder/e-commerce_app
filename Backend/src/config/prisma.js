07ec1cb7bd3b69da131b4d7d9952a61278a9dc42
khushthecoder <chaudharikhush0711@gmail.com>
2025-10-06 23:39:35 +0530

    coment

--- Backend/src/config/prisma.js
+++ Backend/src/config/prisma.js
@@ -1,4 +1,5 @@
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-module.exports = prisma;
+module.exports = prisma;    
+// ye prisma client ko baad me routes me import krke use krna hai database operations k liye
